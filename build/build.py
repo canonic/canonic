696374db4c18805220904f9fbbb42e8b5001d1f4
haikü <olliedawes@gmail.com>
2022-01-19 17:46:11 +0000

    Update build script for app.canonic.com
    Also automatically update the wasm and js file hashes in the index.html file

--- build/build.py
+++ build/build.py
@@ -19,6 +19,7 @@ import subprocess
 import sys
 import time
 import typing
+from urllib.request import urlopen
 
 NO_DEPLOY = '--no-deploy'
 
@@ -39,14 +40,24 @@ PROJECT_PATH = CANONIC_SRC_DIR / 'canonic.pro'
 INDEX_HTML_FILE_NAME = 'index.html'
 QT_LOADER_JS_FILE_NAME = 'qtloader.js'
 
-AWS_BUCKET_NAME = 'www.canonic.com'
-AWS_CF_DISTRIBUTION_ID = 'E783CEX3P81S7'
+AWS_BUCKET_NAME = 'app.canonic.com'
+AWS_CF_DISTRIBUTION_ID = 'ED71MFRDRQ0UE'
 
 SITEMAP_FILE_NAME = 'sitemap.xml'
 WASM_BUILD_NAME = 'canonic.wasm'
 JS_BUILD_NAME = 'canonic.js'
 DEV_HTTPS_KEY_FILE = CANONIC_SRC_DIR / 'build/key.qml'
 DEV_HTTPS_CERT_FILE = CANONIC_SRC_DIR / 'build/cert.qml'
+
+JS_FILE_MD5_HASH_RELEASE_MARKER = 'var JS_FILE_MD5_HASH_RELEASE = "'
+JS_FILE_MD5_HASH_DEBUG_MARKER = 'var JS_FILE_MD5_HASH_DEBUG = "'
+WASM_FILE_MD5_HASH_RELEASE_MARKER = 'var WASM_FILE_MD5_HASH_RELEASE = "'
+WASM_FILE_MD5_HASH_DEBUG_MARKER = 'var WASM_FILE_MD5_HASH_DEBUG = "'
+
+JS_FILE_MD5_HASH_RELEASE_REPLACEMENT = 'var JS_FILE_MD5_HASH_RELEASE = "{}";'
+JS_FILE_MD5_HASH_DEBUG_REPLACEMENT = 'var JS_FILE_MD5_HASH_DEBUG = "{}";'
+WASM_FILE_MD5_HASH_RELEASE_REPLACEMENT = 'var WASM_FILE_MD5_HASH_RELEASE = "{}";'
+WASM_FILE_MD5_HASH_DEBUG_REPLACEMENT = 'var WASM_FILE_MD5_HASH_DEBUG = "{}";'
 
 if 'EMSDK' not in os.environ:
     os.chdir(EMSCRIPTEN_DIR)
@@ -111,6 +122,41 @@ def TimestampISO8601(t):
   return time.strftime('%Y-%m-%dT%H:%M:%SZ', time.gmtime(t))
 
 
+def inplace_replace(filename: str, old_strings: typing.List[str], new_strings: typing.List[str]) -> None:
+    """ Replace a string occurrence in a file inplace."""
+
+    if len(old_strings) != len(new_strings):
+        raise ValueError("string lists must be the same length")
+
+    with open(filename) as f:
+        contents = f.read()
+
+    with open(filename, 'w') as f:
+
+        for i, old_string in enumerate(old_strings):
+            contents = contents.replace(old_string, new_strings[i])
+
+        f.write(contents)
+
+
+def get_old_hashes() -> typing.Tuple[str, str, str, str]:
+    old_index_html_file = str(urlopen("http://{}/{}".format(AWS_BUCKET_NAME, INDEX_HTML_FILE_NAME)).read(), 'utf8')
+
+    i = old_index_html_file.index(JS_FILE_MD5_HASH_RELEASE_MARKER) + len(JS_FILE_MD5_HASH_RELEASE_MARKER)
+    old_js_file_md5_release = old_index_html_file[i: old_index_html_file.index('"', i)]
+
+    i = old_index_html_file.index(JS_FILE_MD5_HASH_DEBUG_MARKER) + len(JS_FILE_MD5_HASH_DEBUG_MARKER)
+    old_js_file_md5_debug = old_index_html_file[i: old_index_html_file.index('"', i)]
+
+    i = old_index_html_file.index(WASM_FILE_MD5_HASH_RELEASE_MARKER) + len(WASM_FILE_MD5_HASH_RELEASE_MARKER)
+    old_wasm_file_md5_release = old_index_html_file[i: old_index_html_file.index('"', i)]
+
+    i = old_index_html_file.index(WASM_FILE_MD5_HASH_DEBUG_MARKER) + len(WASM_FILE_MD5_HASH_DEBUG_MARKER)
+    old_wasm_file_md5_debug = old_index_html_file[i: old_index_html_file.index('"', i)]
+
+    return old_js_file_md5_release, old_js_file_md5_debug, old_wasm_file_md5_release, old_wasm_file_md5_debug
+
+
 if BUILD_DIR.exists():
     print("Cleaning '{}'".format(BUILD_DIR))
     shutil.rmtree(BUILD_DIR)
@@ -145,7 +191,7 @@ shutil.copy(qt_loader_js_src_path, qt_loader_js_build_path)
 
 os.chdir(OUTPUT_DIR)
 
-# Calculate the md5 hash of the wasm  file
+# Calculate the md5 hash of the wasm file
 wasm_file_md5 = hashlib.md5(open(OUTPUT_DIR / WASM_BUILD_NAME,'rb').read()).hexdigest()
 wasm_file_name = 'canonic.{}.{}.wasm'.format(BUILD_SUFFIX, wasm_file_md5)
 
@@ -154,7 +200,7 @@ with open(SITEMAP_FILE_NAME, 'w') as f:
     f.write(f"""<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <url>
-        <loc>https://www.canonic.com/</loc>
+        <loc>https://app.canonic.com/</loc>
         <lastmod>{TimestampISO8601(time.time())}</lastmod>
         <priority>1.0</priority>
     </url>
@@ -176,11 +222,31 @@ js_file_name = 'canonic.{}.{}.js'.format(BUILD_SUFFIX, js_file_md5)
 print("copying '{}' to '{}'".format(JS_BUILD_NAME, js_file_name))
 shutil.copy(OUTPUT_DIR / JS_BUILD_NAME, OUTPUT_DIR / js_file_name)
 
+# Update the index.html file with the new hashes
+old_js_file_md5_release, old_js_file_md5_debug, old_wasm_file_md5_release, old_wasm_file_md5_debug = get_old_hashes()
+inplace_replace(index_html_build_path,
+                [
+                    JS_FILE_MD5_HASH_RELEASE_REPLACEMENT.format(''),
+                    JS_FILE_MD5_HASH_DEBUG_REPLACEMENT.format(''),
+                    WASM_FILE_MD5_HASH_RELEASE_REPLACEMENT.format(''),
+                    WASM_FILE_MD5_HASH_DEBUG_REPLACEMENT.format('')
+                ],
+                [
+                    JS_FILE_MD5_HASH_RELEASE_REPLACEMENT.format(old_js_file_md5_release if DEBUG_BUILD else js_file_md5),
+                    JS_FILE_MD5_HASH_DEBUG_REPLACEMENT.format(js_file_md5 if DEBUG_BUILD else old_js_file_md5_debug),
+                    WASM_FILE_MD5_HASH_RELEASE_REPLACEMENT.format(old_wasm_file_md5_release if DEBUG_BUILD else wasm_file_md5),
+                    WASM_FILE_MD5_HASH_DEBUG_REPLACEMENT.format(wasm_file_md5 if DEBUG_BUILD else old_wasm_file_md5_debug)
+                ])
 
 if NO_DEPLOY not in sys.argv:
     os.chdir(OUTPUT_DIR)
 
     print("Starting file uploads")
+
+    upload_file(INDEX_HTML_FILE_NAME, AWS_BUCKET_NAME, extra_args={
+        'ACL': 'public-read',
+        'ContentType': 'text/html',
+    })
 
     upload_file(SITEMAP_FILE_NAME, AWS_BUCKET_NAME, extra_args={
             'ACL': 'public-read',
@@ -208,6 +274,7 @@ if NO_DEPLOY not in sys.argv:
 
     print("Invalidating cloudfront content")
     invalidation_id = create_invalidation(['/{}'.format(SITEMAP_FILE_NAME),
+                                           '/{}'.format(INDEX_HTML_FILE_NAME),
                                            '/qtloader.js'], AWS_CF_DISTRIBUTION_ID)
 
     print("invalidation_id:", invalidation_id)
